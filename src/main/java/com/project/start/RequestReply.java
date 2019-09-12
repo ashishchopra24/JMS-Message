d46a258cff8301b0d869e06f0c9ab664066c550c
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2019-09-12 23:34:51 +0530

    ExpiryQueue

--- src/main/java/com/project/start/RequestReply.java
+++ src/main/java/com/project/start/RequestReply.java
@@ -11,27 +11,22 @@ public class RequestReply {
 
         InitialContext initialContext=new InitialContext();
         Queue inboundQueue= (Queue) initialContext.lookup("queue/inboundQueue");
-        Queue outboundQueue= (Queue) initialContext.lookup("queue/outboundQueue");
+        Queue expiryQueue= (Queue) initialContext.lookup("queue/expiryQueue");
 
         try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
             JMSContext jmsContext = cf.createContext()
         ){
             JMSProducer producer=jmsContext.createProducer();
-           // producer.setTimeToLive(2000);
-            //TemporaryQueue outboundQueue=jmsContext.createTemporaryQueue();
+            producer.setTimeToLive(2000);
             TextMessage message=jmsContext.createTextMessage("THIS IS A REQUEST MESSAGE");
-            message.setJMSReplyTo(outboundQueue);
             producer.send(inboundQueue,message);
-            //Thread.sleep(4000);
+            Thread.sleep(4000);
+
             JMSConsumer consumer=jmsContext.createConsumer(inboundQueue);
             TextMessage msgReceived=(TextMessage) consumer.receive(5000);
-            System.out.println(msgReceived.getText());
+            System.out.println(msgReceived);
 
-            JMSProducer replyProducer=jmsContext.createProducer();
-            replyProducer.send(msgReceived.getJMSReplyTo(),"REPLY SENT BACK");
-
-            JMSConsumer replyConsumer=jmsContext.createConsumer(outboundQueue);
-            System.out.println(replyConsumer.receiveBody(String.class));
+            System.out.println(jmsContext.createConsumer(expiryQueue).receiveBody(String.class));
 
 
         }
