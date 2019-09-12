cee27be3b0bfaaf1a1647ca8d7553f4b3b67e2cb
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2019-09-12 22:32:05 +0530

    Request Request-Reply

---
+++ src/main/java/com/project/start/RequestReply.java
@@ -0,0 +1,38 @@
+package com.project.start;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class RequestReply {
+    public static void main(String[] args) throws NamingException, JMSException {
+
+        InitialContext initialContext=new InitialContext();
+        Queue inboundQueue= (Queue) initialContext.lookup("queue/inboundQueue");
+        Queue outboundQueue= (Queue) initialContext.lookup("queue/outboundQueue");
+
+        try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+            JMSContext jmsContext = cf.createContext()
+        ){
+            JMSProducer producer=jmsContext.createProducer();
+            //TemporaryQueue outboundQueue=jmsContext.createTemporaryQueue();
+            TextMessage message=jmsContext.createTextMessage("THIS IS A REQUEST MESSAGE");
+            message.setJMSReplyTo(outboundQueue);
+            producer.send(inboundQueue,message);
+
+            JMSConsumer consumer=jmsContext.createConsumer(inboundQueue);
+            TextMessage msgReceived=(TextMessage) consumer.receive();
+            System.out.println(msgReceived.getText());
+
+            JMSProducer replyProducer=jmsContext.createProducer();
+            replyProducer.send(msgReceived.getJMSReplyTo(),"REPLY SENT BACK");
+
+            JMSConsumer replyConsumer=jmsContext.createConsumer(outboundQueue);
+            System.out.println(replyConsumer.receiveBody(String.class));
+
+
+        }
+    }
+}
