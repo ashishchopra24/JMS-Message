4cd5aef9d501ac538bc6eed1f267c51db06384c9
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2019-09-12 23:19:27 +0530

    CorelationId

--- src/main/java/com/project/start/RequestReply.java
+++ src/main/java/com/project/start/RequestReply.java
@@ -5,32 +5,43 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import javax.jms.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RequestReply {
     public static void main(String[] args) throws NamingException, JMSException {
 
         InitialContext initialContext=new InitialContext();
         Queue inboundQueue= (Queue) initialContext.lookup("queue/inboundQueue");
-        Queue outboundQueue= (Queue) initialContext.lookup("queue/outboundQueue");
+
 
         try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
             JMSContext jmsContext = cf.createContext()
         ){
             JMSProducer producer=jmsContext.createProducer();
-            //TemporaryQueue outboundQueue=jmsContext.createTemporaryQueue();
+            TemporaryQueue outboundQueue=jmsContext.createTemporaryQueue();
             TextMessage message=jmsContext.createTextMessage("THIS IS A REQUEST MESSAGE");
             message.setJMSReplyTo(outboundQueue);
             producer.send(inboundQueue,message);
+            System.out.println(message.getJMSMessageID());
+
+            Map<String,TextMessage> requestMessages=new HashMap<>();
+            requestMessages.put(message.getJMSMessageID(),message);
+
 
             JMSConsumer consumer=jmsContext.createConsumer(inboundQueue);
             TextMessage msgReceived=(TextMessage) consumer.receive();
             System.out.println(msgReceived.getText());
 
             JMSProducer replyProducer=jmsContext.createProducer();
-            replyProducer.send(msgReceived.getJMSReplyTo(),"REPLY SENT BACK");
+            TextMessage replyMessage=jmsContext.createTextMessage("REPLY SENT BACK");
+            replyMessage.setJMSCorrelationID(msgReceived.getJMSMessageID());
+            replyProducer.send(msgReceived.getJMSReplyTo(),replyMessage);
 
             JMSConsumer replyConsumer=jmsContext.createConsumer(outboundQueue);
-            System.out.println(replyConsumer.receiveBody(String.class));
+            TextMessage replyReceived=(TextMessage)replyConsumer.receive();
+            System.out.println(replyReceived.getJMSCorrelationID());
+            System.out.println(requestMessages.get(replyReceived.getJMSCorrelationID()).getText());
 
 
         }
